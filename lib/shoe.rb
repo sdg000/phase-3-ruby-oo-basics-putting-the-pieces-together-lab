b6742326c835046d2c849c7cf86aef6493f70b30
Seth David Gyimah <esec26@gmail.com>
2022-08-24 12:17:17 +0000

    Ruby oo putting it all together

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
-# Make your shoe class here!
+class Shoe
+
+    attr_reader :brand
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble 
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+    # def condition
+    #     "tattered"
+    # end
+end
+
+# shoe = Shoe.new
+# shoe.condition = "new"
